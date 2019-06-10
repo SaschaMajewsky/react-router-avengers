7a92e97e43441668a52e031fac32d65a55a2fab4
Sascha <sascha.majewsky93@gmx.de>
2019-06-10 17:08:40 +0200

    finished route of list of avengers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react'; 
 import './App.css';
 import { Route } from 'react-router-dom';
+import Home from './components/Home';
+import AvengersList from './components/AvengersList';
+import avengers from './data';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      avengers: avengers
+    }
+  }
 
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/avengers" component={AvengersList} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Avengers Database:</h1>
+            <h4>Information about the Avengers delivered to your doorstep!</h4>
+        </div>
+    )
+}
+
+export default Home;
