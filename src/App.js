489702f4f1fa99c75ab589b334c943d6854b0fcb
Sascha <sascha.majewsky93@gmx.de>
2019-06-10 18:55:15 +0200

    added a navbar and further dynamic routing when clicking on an avenger through the Link component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import AvengersList from './components/AvengersList';
 import AvengerPage from './components/AvengerPage';
@@ -17,6 +17,14 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <ul className="navbar">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/avengers">Avengers</Link>
+          </li>
+        </ul>
         <Route exact path="/" component={Home} />
         <Route exact path="/avengers" component={AvengersList} />
         <Route path="/avengers/:id" component={AvengerPage} />
