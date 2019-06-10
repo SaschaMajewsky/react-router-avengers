467d9ac93c9aaf1ff87a62017209d3da929b41b9
Sascha <sascha.majewsky93@gmx.de>
2019-06-10 18:43:44 +0200

    added the dynamic route to display a single avenger

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import Home from './components/Home';
 import AvengersList from './components/AvengersList';
+import AvengerPage from './components/AvengerPage';
 import avengers from './data';
 
 class App extends React.Component {
@@ -17,7 +18,8 @@ class App extends React.Component {
     return (
       <div className="App">
         <Route exact path="/" component={Home} />
-        <Route path="/avengers" component={AvengersList} />
+        <Route exact path="/avengers" component={AvengersList} />
+        <Route path="/avengers/:id" component={AvengerPage} />
       </div>
     );
   }
